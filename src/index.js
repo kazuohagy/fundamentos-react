831434da4777968300be4ed711bf8f16e2ebb16e
Gustavo Kazuo <kazucalado@gmail.com>
2022-01-24 14:38:44 -0300

    Projeto Criado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import "./index.css";
 import ReactDOM from "react-dom";
 import React from "react";
 
-const tag = <strong>Olá React!</strong>;
+const tag = (
+  <h1>
+    <strong>Olá React!</strong>
+  </h1>
+);
 ReactDOM.render(<div>{tag}</div>, document.getElementById("root"));
