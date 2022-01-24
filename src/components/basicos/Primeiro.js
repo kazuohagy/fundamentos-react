df4c934d699d4a129a3e4c524b10f65c91a9c69d
Gustavo Kazuo <kazucalado@gmail.com>
2022-01-24 17:03:53 -0300

    react

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,8 +1,9 @@
 import React from "react";
 export default function Primeiro() {
+  const msg = "Seja bem vindo(a)!";
   return (
     <div>
-      <h2>Primeiro Componente</h2>;
+      <h2>Primeiro Componente</h2>;<p>{msg}</p>
     </div>
   );
 }
